0518daac18f6de73d99e26d382957d933b21499a
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-06-17 10:51:45 -0300

    Navigation files added

--- screens/AddPlaces.js
+++ screens/AddPlaces.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { View } from "react-native";
-
-// import { Container } from './styles';
+import PlaceForm from "../components/PlaceForm";
 
 const AddPlaces = () => {
-  return <View />;
+  return <PlaceForm />;
 };
 
 export default AddPlaces;
