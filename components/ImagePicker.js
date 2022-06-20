7ebb8662531a502265e9a1a64c6c7af81401c8d9
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-06-20 09:24:08 -0300

    Image preview added

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,23 +1,77 @@
 import React from "react";
-import { View, Button } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
-import ButtonUI from "./UI/Button";
+import { View, Button, Alert, Image, Text, StyleSheet } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 const ImagePicker = () => {
+  const [cameraPermission, requestPermission] = useCameraPermissions();
+  const [image, setImage] = React.useState(null);
+
+  const verifyPermissions = async () => {
+    if (cameraPermission.status === PermissionStatus.UNDETERMINED) {
+      const permisisonResponse = await requestPermission();
+
+      return permisisonResponse.granted;
+    }
+
+    if (cameraPermission.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Camera Permission",
+        "Camera permission is required to use this feature. Please allow it in settings.",
+        [{ text: "OK" }]
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   const takeImageHandler = async () => {
+    const hasPermisison = await verifyPermissions();
+
+    if (!hasPermisison) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5, // remember this for weak devices
     });
+
+    setImage(image.uri);
   };
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>
+        {image ? (
+          <Image source={{ uri: image }} style={styles.image} />
+        ) : (
+          <Text>No image selected</Text>
+        )}
+      </View>
       <Button title={"Take Photo"} onPress={takeImageHandler} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e3e3e3",
+    marginHorizontal: 24,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
 
 export default ImagePicker;
