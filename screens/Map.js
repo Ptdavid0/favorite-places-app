1580b055368802ac465b29b67c2836f7ce8929de
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-06-20 15:06:34 -0300

    Markes added

--- screens/Map.js
+++ screens/Map.js
@@ -3,13 +3,32 @@ import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = React.useState(null);
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView initialRegion={region} style={styles.mapPreview} />;
+
+  const selectLocationHandler = (event) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    setSelectedLocation({
+      latitude: latitude,
+      longitude: longitude,
+    });
+  };
+
+  return (
+    <MapView
+      initialRegion={region}
+      style={styles.mapPreview}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && <Marker coordinate={selectedLocation} />}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
